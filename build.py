515d2a290fff1a3cc71bcf6236973462b375b6b7
moscamich <moscamich@gmail.com>
2025-07-14 04:13:01 -0400

    build for darwin and linux

--- build.py
+++ build.py
@@ -1,11 +1,23 @@
 from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
 import pybind11
+import platform
+import sys
+
+extra_compile_args = ["-std=c++17"]
+extra_link_args = []
+
+# Add macOS-specific flags only if on macOS
+if sys.platform == "darwin":
+    extra_compile_args += ["-mmacosx-version-min=11.0"]
+    extra_link_args += [
+        "-stdlib=libc++",
+        "-mmacosx-version-min=11.0",
+    ]
 
 ext_modules = [
     Extension(
-        # "gpmf_parser",
-        "py_gpmf_parser.gpmf_parser",  # <-- important change
+        "py_gpmf_parser.gpmf_parser",
         sources=[
             "src/gpmf_bindings.cpp",
             "gpmf-parser/GPMF_parser.c",
@@ -18,14 +30,8 @@ ext_modules = [
             "gpmf-parser/demo",
         ],
         language="c++",
-        extra_compile_args=[
-            "-std=c++17",
-            "-mmacosx-version-min=11.0",
-        ],
-        extra_link_args=[
-            "-stdlib=libc++",
-            "-mmacosx-version-min=11.0",
-        ],
+        extra_compile_args=extra_compile_args,
+        extra_link_args=extra_link_args,
     ),
 ]
 
@@ -33,4 +39,3 @@ setup(
     ext_modules=ext_modules,
     cmdclass={"build_ext": build_ext},
 )
-
